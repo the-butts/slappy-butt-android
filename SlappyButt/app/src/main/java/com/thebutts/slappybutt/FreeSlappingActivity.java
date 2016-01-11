6c00b5ccdf535e3424597f46b5f785767d396e85
Ivo Paounov <ivo.paunov@gmail.com>
2016-01-11 13:24:27 +0200

    Animations fixes. Background change with double tap - animated.

--- SlappyButt/app/src/main/java/com/thebutts/slappybutt/FreeSlappingActivity.java
+++ SlappyButt/app/src/main/java/com/thebutts/slappybutt/FreeSlappingActivity.java
@@ -1,11 +1,16 @@
 package com.thebutts.slappybutt;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.drawable.AnimationDrawable;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.support.v4.view.GestureDetectorCompat;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.GestureDetector;
@@ -14,11 +19,15 @@ import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.thebutts.slappybutt.Helpers.*;
+
+import java.util.Random;
 
 public class FreeSlappingActivity extends Activity implements SensorEventListener {
 
@@ -43,12 +52,32 @@ public class FreeSlappingActivity extends Activity implements SensorEventListene
     AnimationDrawable rightButtPinchOut;
     AnimationDrawable leftButtPinchOut;
 
+    AnimationDrawable rightShake;
+    AnimationDrawable leftShake;
+
+    Animation fadeIn;
+    Animation fadeOut;
+
+
+    ImageView mBodyTop;
     ImageView mLevtButtContainer;
     ImageView mRightButtContainer;
 
     FrameLayout mSlappinngFrame;
+    FrameLayout mActivityLayout;
 
     Integer mScreenWidth;
+
+    Vibrator vibrator;
+
+    TypedArray mBackgroundImages;
+
+    private SensorManager senSensorManager;
+    private Sensor senAccelerometer;
+
+    private long lastUpdate = 0;
+    private float last_x, last_y, last_z;
+    private static final int SHAKE_THRESHOLD = 800;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,9 +88,18 @@ public class FreeSlappingActivity extends Activity implements SensorEventListene
 
         setContentView(R.layout.activity_free_slapping);
 
-        getScreenWidth();
-        SetupAnimations();
-        setupUiEvents();
+        this.getScreenWidth();
+        this.SetupAnimations();
+        this.setupUiEvents();
+        this.SetupSensors();
+    }
+
+    void SetupSensors() {
+        senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
     }
 
     void SetupAnimations() {
@@ -73,7 +111,17 @@ public class FreeSlappingActivity extends Activity implements SensorEventListene
         mLevtButtContainer.setBackgroundResource(R.drawable.first_left_initial);
         mLevtButtContainer.setLayerType(View.LAYER_TYPE_HARDWARE, null);
 
+        mBodyTop = (ImageView) findViewById(R.id.ivBodyTop);
+
         mSlappinngFrame = (FrameLayout) findViewById(R.id.slappingFrame);
+        mActivityLayout = (FrameLayout) findViewById(R.id.freeSlapActivityLayout);
+
+        mBackgroundImages = getResources().obtainTypedArray(R.array.background_images);
+
+        fadeIn = AnimationUtils.loadAnimation(FreeSlappingActivity.this, R.anim.fade_in);
+        fadeOut = AnimationUtils.loadAnimation(FreeSlappingActivity.this, R.anim.fade_out);
+
+        setRandomBackGround();
     }
 
     void getScreenWidth() {
@@ -83,7 +131,37 @@ public class FreeSlappingActivity extends Activity implements SensorEventListene
         mScreenWidth = metrics.widthPixels;
     }
 
+    void setRandomBackGround(){
+        Random rand = new Random();
+
+        final int index = rand.nextInt(mBackgroundImages.length());
+
+        mActivityLayout.startAnimation(fadeOut);
+        mActivityLayout.setBackgroundResource(mBackgroundImages.getResourceId(index, -1));
+        mActivityLayout.startAnimation(fadeIn);
+    }
+
     void setupUiEvents() {
+
+        final GestureDetectorCompat doubleTapGestureDetector = new GestureDetectorCompat(this, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onDoubleTap(MotionEvent e) {
+
+                FreeSlappingActivity.this.setRandomBackGround();
+                return true;
+            }
+        });
+
+        mBodyTop.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                doubleTapGestureDetector.onTouchEvent(event);
+
+                return true;
+            }
+        });
+
         final GestureDetector gestureDetector = new GestureDetector(this, new SwipeGestureDetector() {
             @Override
             public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
@@ -159,8 +237,7 @@ public class FreeSlappingActivity extends Activity implements SensorEventListene
 
                 int action = event.getAction();
 
-                switch(action & MotionEvent.ACTION_MASK)
-                {
+                switch (action & MotionEvent.ACTION_MASK) {
                     case MotionEvent.ACTION_POINTER_DOWN:
                         FreeSlappingActivity.this.onClickBtnLeftButt();
                         FreeSlappingActivity.this.onClickBtnRightButt();
@@ -189,7 +266,7 @@ public class FreeSlappingActivity extends Activity implements SensorEventListene
         mLevtButtContainer.setBackgroundResource(R.drawable.anim_first_left_pinch_out);
         leftButtPinchOut = (AnimationDrawable) mLevtButtContainer.getBackground();
 
-        this.stopAllActiveAnimations();
+        this.stopAllActiveAnimations("onHuc");
 
         rightButtPinchOut.start();
         leftButtPinchOut.start();
@@ -202,7 +279,7 @@ public class FreeSlappingActivity extends Activity implements SensorEventListene
         mLevtButtContainer.setBackgroundResource(R.drawable.anim_first_left_pinch_in);
         leftButtPinchIn = (AnimationDrawable) mLevtButtContainer.getBackground();
 
-        this.stopAllActiveAnimations();
+        this.stopAllActiveAnimations("onHuc");
 
         rightButtPinchIn.start();
         leftButtPinchIn.start();
@@ -215,7 +292,7 @@ public class FreeSlappingActivity extends Activity implements SensorEventListene
         mLevtButtContainer.setBackgroundResource(R.drawable.anim_first_left_flint_up);
         leftButtSFlintUp = (AnimationDrawable) mLevtButtContainer.getBackground();
 
-        this.stopAllActiveAnimations();
+        this.stopAllActiveAnimations("onHuc");
 
         rightButtFlintUp.start();
         leftButtSFlintUp.start();
@@ -228,7 +305,7 @@ public class FreeSlappingActivity extends Activity implements SensorEventListene
         mLevtButtContainer.setBackgroundResource(R.drawable.anim_first_left_flint_down);
         leftButtFlintDown = (AnimationDrawable) mLevtButtContainer.getBackground();
 
-        this.stopAllActiveAnimations();
+        this.stopAllActiveAnimations("onHuc");
 
         rightButtFlintDown.start();
         leftButtFlintDown.start();
@@ -241,7 +318,7 @@ public class FreeSlappingActivity extends Activity implements SensorEventListene
         mLevtButtContainer.setBackgroundResource(R.drawable.anim_first_left_flint_right);
         leftButtFlintRight = (AnimationDrawable) mLevtButtContainer.getBackground();
 
-        this.stopAllActiveAnimations();
+        this.stopAllActiveAnimations("onHuc");
 
         rightButtFlintRight.start();
         leftButtFlintRight.start();
@@ -254,19 +331,44 @@ public class FreeSlappingActivity extends Activity implements SensorEventListene
         mLevtButtContainer.setBackgroundResource(R.drawable.anim_first_left_flint_left);
         leftButtSFlintLeft = (AnimationDrawable) mLevtButtContainer.getBackground();
 
-        this.stopAllActiveAnimations();
+        this.stopAllActiveAnimations("onHuc");
 
         rightButtFlintLeft.start();
         leftButtSFlintLeft.start();
+    }
+
+    private void onClickShake() {
+        mRightButtContainer.setBackgroundResource(R.drawable.anim_first_right_shake);
+        rightShake = (AnimationDrawable) mRightButtContainer.getBackground();
+
+        mLevtButtContainer.setBackgroundResource(R.drawable.anim_first_left_shake);
+        leftShake = (AnimationDrawable) mLevtButtContainer.getBackground();
+
+        this.stopAllActiveAnimations("onHuc");
+
+        rightShake.start();
+        leftShake.start();
+
+        long[] pattern = new long[]{75, 225, 75, 75, 75, 75, 75, 225, 75, 225, 75, 225, 75, 75, 75, 225, 75, 225, 75, 75, 75, 75, 75, 225, 75, 225, 75, 225, 75, 75, 75, 225, 75, 225, 75, 75, 75, 75, 75, 225, 75, 225, 150, 150, 75, 75, 75, 225, 75, 375, 75, 75, 75, 75, 75, 225, 75, 225, 75, 225};
+
+        vibrator.vibrate(pattern, -1);
     }
 
     private void onClickBtnRightButt() {
         mRightButtContainer.setBackgroundResource(R.drawable.anim_first_right_butt_slap);
         rightButtSlappAnimation = (AnimationDrawable) mRightButtContainer.getBackground();
 
-        if (rightButtSlappAnimation.isRunning()) {
-            rightButtSlappAnimation.stop();
-        }
+        this.stopAllActiveAnimations("onRight");
+
+//        if (rightButtSlappAnimation.isRunning()) {
+//
+//            rightButtSlappAnimation.stop();
+//            vibrator.cancel();
+//        }
+//
+//        if (leftButtSlappAnimation == null || leftButtSlappAnimation.isRunning()) {
+//            mLevtButtContainer.setBackgroundResource(R.drawable.first_left_initial);
+//        }
 
         rightButtSlappAnimation.start();
     }
@@ -275,60 +377,137 @@ public class FreeSlappingActivity extends Activity implements SensorEventListene
         mLevtButtContainer.setBackgroundResource(R.drawable.anim_first_left_butt_slap);
         leftButtSlappAnimation = (AnimationDrawable) mLevtButtContainer.getBackground();
 
-        if (leftButtSlappAnimation.isRunning()) {
-            leftButtSlappAnimation.stop();
-        }
+        this.stopAllActiveAnimations("onLeft");
+
+//        if (leftButtSlappAnimation.isRunning()) {
+//            leftButtSlappAnimation.stop();
+//            vibrator.cancel();
+//        }
+//
+//        if (rightButtSlappAnimation == null || !rightButtSlappAnimation.isRunning()) {
+//            mRightButtContainer.setBackgroundResource(R.drawable.first_right_initial);
+//        }
 
         leftButtSlappAnimation.start();
     }
 
-    private void stopAllActiveAnimations() {
-        if (leftButtSlappAnimation != null && leftButtSlappAnimation.isRunning()) {
-            leftButtSlappAnimation.stop();
-        }
-
-        if (rightButtSlappAnimation != null && rightButtSlappAnimation.isRunning()) {
-            rightButtSlappAnimation.stop();
-        }
+    private void stopAllActiveAnimations(String clickedOn) {
 
         if (rightButtFlintLeft != null && (rightButtFlintLeft.isRunning() || leftButtSFlintLeft.isRunning())) {
+            //  setInitialButts();
             rightButtFlintLeft.stop();
             leftButtSFlintLeft.stop();
         }
 
         if (rightButtFlintDown != null && (rightButtFlintDown.isRunning() || leftButtFlintDown.isRunning())) {
+            // setInitialButts();
             rightButtFlintDown.stop();
             leftButtFlintDown.stop();
         }
 
         if (rightButtFlintRight != null && (rightButtFlintRight.isRunning() || leftButtFlintRight.isRunning())) {
+            //  setInitialButts();
             rightButtFlintRight.stop();
             leftButtFlintRight.stop();
         }
 
         if (rightButtFlintUp != null && (rightButtFlintUp.isRunning() || leftButtSFlintUp.isRunning())) {
+            // setInitialButts();
             rightButtFlintUp.stop();
             leftButtSFlintUp.stop();
         }
 
         if (rightButtPinchIn != null && (rightButtPinchIn.isRunning() || leftButtPinchIn.isRunning())) {
+            //setInitialButts();
             rightButtPinchIn.stop();
             leftButtPinchIn.stop();
         }
 
         if (rightButtPinchOut != null && (rightButtPinchOut.isRunning() || leftButtPinchOut.isRunning())) {
+            // setInitialButts();
             rightButtPinchOut.stop();
             leftButtPinchOut.stop();
         }
+
+        if (rightShake != null && (rightShake.isRunning() || leftShake.isRunning())) {
+            //setInitialButts();
+            rightShake.stop();
+            leftShake.stop();
+            vibrator.cancel();
+        }
+
+        if (leftButtSlappAnimation != null && leftButtSlappAnimation.isRunning()) {
+            if (!clickedOn.equals("onRight")) {
+                leftButtSlappAnimation.stop();
+            }
+        }
+
+        if (rightButtSlappAnimation != null && rightButtSlappAnimation.isRunning()) {
+
+            if (!clickedOn.equals("onLeft")) {
+                rightButtSlappAnimation.stop();
+            }
+        }
+
+        if (clickedOn.equals("onRight") && (leftButtSlappAnimation == null || !leftButtSlappAnimation.isRunning())) {
+            mLevtButtContainer.setBackgroundResource(R.drawable.first_left_initial);
+        }
+
+        if (clickedOn.equals("onLeft") && (rightButtSlappAnimation == null || !rightButtSlappAnimation.isRunning())) {
+            mRightButtContainer.setBackgroundResource(R.drawable.first_right_initial);
+        }
+
+    }
+
+    private void setInitialButts() {
+        mRightButtContainer.setBackgroundResource(R.drawable.first_right_initial);
+        mLevtButtContainer.setBackgroundResource(R.drawable.first_left_initial);
     }
 
     @Override
-    public void onSensorChanged(SensorEvent event) {
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Sensor mySensor = sensorEvent.sensor;
 
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
+
+            long curTime = System.currentTimeMillis();
+
+            if ((curTime - lastUpdate) > 300) {
+                long diffTime = (curTime - lastUpdate);
+                lastUpdate = curTime;
+
+                float speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000;
+
+                if (speed > SHAKE_THRESHOLD) {
+
+                    FreeSlappingActivity.this.onClickShake();
+                    // Toast.makeText(this, "Shake it baby", Toast.LENGTH_SHORT).show();
+                }
+
+                last_x = x;
+                last_y = y;
+                last_z = z;
+            }
+        }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        senSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 }
